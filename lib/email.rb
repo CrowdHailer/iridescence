9f232c2004dce4216af881d87dfa21d3956e2530
Peter Saxton <peterhsaxton@gmail.com>
2015-03-29 15:06:56 +0100

    create email type

---
+++ lib/email.rb
@@ -0,0 +1,46 @@
+module Stash
+  ItemError = Class.new(StandardError)
+
+  def dump(item)
+    raise ItemError.new unless item.is_a?(self)
+    item.dump
+  end
+end
+
+class Email
+  extend Stash
+  include Comparable
+  Invalid = Class.new(StandardError)
+
+  def initialize(value)
+    self.value = value
+  end
+
+  def self.load(string)
+    new(string)
+  end
+
+  def to_s
+    value
+  end
+  alias_method :to_str, :to_s
+  alias_method :dump, :to_s
+
+  def value
+    @value
+  end
+
+  def value=(new_value)
+    @value = new_value.strip[/^[^@]+@[^@]+$/] || invalid(new_value)
+  end
+
+  def <=>(other)
+    value <=> other
+  end
+
+  private
+
+  def invalid(bad_format)
+    raise Invalid.new "'#{bad_format}' is not a valid email"
+  end
+end
---
+++ test/email/email_test.rb
@@ -0,0 +1,45 @@
+require_relative '../test_config'
+
+class EmailTest < MiniTest::Test
+  # Construction
+  def test_clears_white_space
+    assert_equal Email.new('test@example.com'), Email.new(' test@example.com')
+  end
+
+  def test_raises_error_for_email_with_no_at
+    assert_raises Email::Invalid do
+      Email.new('bad')
+    end
+  end
+
+  def test_raises_error_for_email_with_two_ats
+    assert_raises Email::Invalid do
+      Email.new('a@b@c.com')
+    end
+  end
+
+  def test_raises_error_when_dumping_not_an_email
+    assert_raises Stash::ItemError do
+      Email.dump('string')
+    end
+  end
+
+  # Comparison
+
+  def test_is_equal_for_same_email
+    assert_equal Email.new('test@example.com'), Email.new('test@example.com')
+  end
+
+  def test_orders_alphabetically
+    assert_equal -1, Email.new('a@example.com') <=> Email.new('b@example.com')
+  end
+
+  def test_shows_as_a_string
+    assert_equal 'test@example.com', "#{Email.new('test@example.com')}"
+  end
+
+  def test_adds_to_strings
+    assert_equal 'test@example.com', ('' + Email.new('test@example.com'))
+  end
+
+end
