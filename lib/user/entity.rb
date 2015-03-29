22ad520ea9f596bea3ea213be37b26670dcbc0d4
Peter Saxton <peterhsaxton@gmail.com>
2015-03-29 14:35:43 +0100

    test entity

--- lib/user/entity.rb
+++ lib/user/entity.rb
@@ -1,5 +1,7 @@
 module User
   class Entity < Errol::Entity
-    entry_accessor  :email
+    entry_accessor  :email,
+                    :password,
+                    :last_login_at
   end
 end
--- test/user/entity_test.rb
+++ test/user/entity_test.rb
@@ -1,10 +1,35 @@
 require_relative '../test_config'
 
+# TODO needs to select what you are testing with. Might be overkill
+module Errol
+  module EnitiyTesting
+    def entry_access(*entries, **options)
+      entries.each do |entry|
+        value = options.fetch(:with) { :test_symbol }
+        define_method :"test_can_access_#{entry}" do
+          record.send "#{entry}=", value
+          assert_equal value, entity.send(entry)
+        end
+
+        define_method :"test_can_set_#{entry}" do
+          entity.send "#{entry}=", value
+          assert_equal value, record.send(entry)
+        end
+      end
+    end
+  end
+end
+
 module User
   class EntityTest < MyTest
+    extend Errol::EnitiyTesting
     def user
       # TODO change to just User
       @user ||= User::Entity.new record
+    end
+
+    def entity
+      user
     end
 
     def record
@@ -16,14 +41,8 @@ module User
       @record = nil
     end
 
-    def test_can_access_email
-      record.email = 'test@example.com'
-      assert_equal 'test@example.com', user.email
-    end
-
-    def test_can_set_email
-      user.email = 'test@example.com'
-      assert_equal 'test@example.com', record.email
-    end
+    entry_access :email, :with => 'test@example.com'
+    entry_access :password, :with => 'pasword'
+    entry_access :last_login_at, :with => Time.at(200)
   end
 end
